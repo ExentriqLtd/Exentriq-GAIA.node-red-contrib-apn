ab9dddbbda0ad4b4473abb86fd848f295aaad9f0
Calogero Crapanzano <calosan@libero.it>
2016-09-01 16:55:58 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exentriq-GAIA.node-red-contrib-apn
+
+## How to create certificates
+### Production certificates
+openssl x509 -in aps_development.cer -inform DER -outform PEM -out cert.pem
+openssl pkcs12 -in private_key_notification.p12 -out key.pem -nodes
+### Sandbox certificates
+openssl s_client -connect gateway.sandbox.push.apple.com:2195 -cert cert.pem -key key.pem
+openssl s_client -connect gateway.push.apple.com:2195 -cert cert.pem -key key.pem
---
+++ apn.js
@@ -0,0 +1,89 @@
+module.exports = function(RED) {
+	
+    function Apn(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+        var apn = require('apn');
+        var fs = require("fs");
+        var util = require('util');
+
+        try {
+        	this.on('input', function(msg) {
+            
+        		var path = config.path;
+                if( path.substr(-1) != "/" ) {
+                	path += "/";
+                }
+                
+                var certificate = path+'cert.pem';
+                var key = path+'key.pem';
+                
+                if(!fs.existsSync(certificate)) {
+                	node.error("File not found: " + certificate, msg);
+                	return;
+            	}
+                if(!fs.existsSync(key)) {
+                	node.error("File not found: " + key, msg);
+                	return;
+            	}
+                
+                var production = true;
+                if (config.hasOwnProperty('destination')) { 
+                	if(config.destination == 'Production'){
+                		production = true;
+                	}
+                	else{
+                		production = false;
+                	}
+                }
+                
+                node.log("Production: " + production);
+                
+                var options = {'cert':certificate, 'key':key, 'production':production };
+
+        	var apnConnection = new apn.Connection(options);
+                var note = new apn.Notification();
+
+                note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+                if(msg.badge!=null){
+                	note.badge = msg.badge;
+                }
+                if(msg.notifitationPayload!=null){
+                	note.payload = msg.notifitationPayload;
+                }
+                else{
+                	note.payload = {'messageFrom': 'Exentriq'};
+                }
+                note.sound = "ping.aiff";
+                note.alert = msg.payload;
+                
+
+                var regTokens = [];
+                if(util.isArray(msg.topic)){
+        			regTokens = msg.topic;
+        		}
+        		else{
+        			regTokens.push(msg.topic);
+        		}
+                var l = regTokens.length;
+                for (var i = 0; i < l; i++) {
+                	try {
+                		var regToken = regTokens[i];
+                    	var myDevice = new apn.Device(regToken);
+                    	apnConnection.pushNotification(note, myDevice);
+					} catch (e) {
+						node.error("Errore during notification sent", e);
+					}
+                	
+                }
+                apnConnection.shutdown();
+                //node.status({});
+        	});
+        }
+    	catch(e){
+    		node.error("ops, there was an error!", msg);
+        }
+    }
+    
+    RED.nodes.registerType("apn",Apn);
+}
