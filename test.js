820bf61443242c6aa9b6d060dd3c56bc13166e9e
adrianotamburo <adriano.tamburo@gmail.com>
2020-10-09 15:09:56 +0200

    Minor fixes

--- test.js
+++ test.js
@@ -1,12 +1,13 @@
 var apn = require('apn');
 
-var path = '/home/calogero/sviluppo/tmp/meet/'
+var path = '/Users/adrianotamburo/Desktop/meet_certificates/voip/'
 var certificate = path+'cert.pem';
 var key = path+'key.pem';
-var options = {'cert':certificate, 'key':key, 'production':true };
+var options = {'cert':certificate, 'key':key, 'production':false };
 var apnConnection = new apn.Connection(options);
 
-let deviceToken = "MHKSJBYScvEdGjgrMSTquutcnMSTKNJsspvjrAOHXImwQprlFAdxojLBiRGfvuTH";
+let deviceToken = "3f6e276249f762742977de77375978ec8096773d26e3d9d358d02b3aac7625b6";
+let deviceTokenPushKit = "5a2a813b9372fa165374ee8cb8c439da4f8fb74880e4b1e83379ead9942ef17f";
 
 var note = new apn.Notification();
 note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
@@ -16,6 +17,6 @@ note.alert = "\uD83D\uDCE7 \u2709 You have a new message";
 note.payload = {'messageFrom': 'John Appleseed'};
 note.topic = "com.exentriq.meet.voip";
 
-var myDevice = new apn.Device(deviceToken);
+var myDevice = new apn.Device(deviceTokenPushKit);
 apnConnection.pushNotification(note, myDevice);
 apnConnection.shutdown();
